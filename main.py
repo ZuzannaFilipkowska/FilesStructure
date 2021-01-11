4662ee21876ecad56a5765c59e1bbebf7215a19b
Zuzanna Filipkowska <zuzanna.filipkowska.stud@pw.edu.pl>
2021-01-11 23:22:03 +0100

    Handled some exceptions

--- main.py
+++ main.py
@@ -11,25 +11,43 @@ def main():
     while answer:
         try:
             path = get_path(working_dir, home)
+
             answer = input(path).split()
+
             if answer[0] == "cd":
                 new_working_dir = cd(answer, working_dir, home)
                 if new_working_dir:
-                    working_dir =  new_working_dir
+                    working_dir = new_working_dir
                 else:
-                    print("Incorrect command. Try again")
+                    print("Incorrect usage of: cd. Try again")
             elif answer[0] == 'mkdir':
                 # make folder
-                Folder(answer[1], working_dir)
+                try:
+                    Folder(answer[1], working_dir)
+                except Exception:
+                    print("Incorrect usage of: mkdir. Try again")
             elif answer[0] == 'mk':
                 # make file
-                File(working_dir, answer[1], answer[2], answer[3])
+                try:
+                    File(working_dir, answer[1], answer[2], answer[3])
+                except Exception:
+                    print("Incorrect usage of: mk. Try again")
             elif answer[0] == 'rm':
                 # delete file or folder
-                rm(answer[1], working_dir)
+                if len(answer) == 2:
+                    to_be_deleted = working_dir.find(answer[1])
+                    if to_be_deleted:
+                        print(rm(to_be_deleted, working_dir, home))
+                    else:
+                        print(f"Error. {answer[1]} - Element does not exist")
+                else:
+                    print("Incorrect usage of: rm. Try again")
             elif answer[0] == 'ls':
                 # print structure
-                ls(working_dir, answer)
+                try:
+                    ls(working_dir, answer)
+                except Exception:
+                    print("Incorrect usage of: ls. Try again")
             elif answer[0] == 'cat':
                 # print file/folder info
                 cat(answer[1])
@@ -49,9 +67,11 @@ def main():
             elif answer[0] == 'exit':
                 # end program
                 answer = False
+            else:
+                print("Command not found")
         except IndexError:
             print("No command was given. Try again.")
-            answer = input(path).split()
+            main()
 
 
 if __name__ == "__main__":
